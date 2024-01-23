e1dc234c049ab9dde0603b656cc09de2543097ac
Kaloyan <igntoxicshadow@gmail.com>
2024-01-23 21:07:13 +0200

    Added code persistence

--- src/lib/components/CodeEditor/CodeEditor.svelte
+++ src/lib/components/CodeEditor/CodeEditor.svelte
@@ -1,24 +1,34 @@
-<script>
+<script lang='ts'>
+	import { onMount } from 'svelte';
 	import FileObject from './FileObject.svelte';
-	import { currentCode } from './editorStore';
+	import { currentFile } from './editorStore';
 	import { FileTypes } from './enums/fileTypes';
+	import { v4 as uuidv4 } from 'uuid';
 
+	onMount(() => {
+		itemMap = buildPathMap(menuItems);
+	})
+
+	let itemMap: any;
 	let menuItems = [
 				{
+					uuid: uuidv4(),
 					label: 'index.js',
 					type: FileTypes.FILE,
-					content: 'This is the content of Chapter 1.'
+					content: 'console.log("test");\nconsole.log("test");\nconsole.log("test");'
 				},
 				{
 					label: 'folder',
 					type: FileTypes.FOLDER,
 					children: [
 						{
+							uuid: uuidv4(),
 							label: 'sub.js',
 							type: FileTypes.FILE,
 							content: 'Content for Chapter 2.'
 						},
 						{
+							uuid: uuidv4(),
 							label: 'sub.js',
 							type: FileTypes.FILE,
 							content: 'Content for Chapter 2.'
@@ -28,16 +38,19 @@
 							type: FileTypes.FOLDER,
 							children: [
 								{
+									uuid: uuidv4(),
 									label: 'sub.js',
 									type: FileTypes.FILE,
 									content: 'Content for Chapter 2.'
 								},
 								{
+									uuid: uuidv4(),
 									label: 'sub.js',
 									type: FileTypes.FILE,
 									content: 'Content for Chapter 2.'
 								},
 								{
+									uuid: uuidv4(),
 									label: 'subjs',
 									type: FileTypes.FILE,
 									content: 'Content for Chapter 2.'
@@ -45,8 +58,20 @@
 							]
 						}
 					]
-				}
+				},
 	];
+
+	function buildPathMap(items: any, map: any = {}) {
+		items.forEach((item: any) => {
+			if (item.uuid) {
+				map[item.uuid as string] = item;
+			}
+			if (item.children && Array.isArray(item.children)) {
+				buildPathMap(item.children, map);
+			}
+		});
+		return map;
+	}
 </script>
 
 <div class="flex border rounded-lg overflow-hidden container min-h-96">
@@ -55,5 +80,7 @@
 		<FileObject {item} />
 	  {/each}
 	</ul>
-	<textarea class="textarea flex-1 resize-none" value={$currentCode} />
+	{#if $currentFile}
+		<textarea class="textarea flex-1 resize-none" bind:value={itemMap[$currentFile].content}/>
+	{/if}
 </div>
--- src/lib/components/CodeEditor/editorStore.ts
+++ src/lib/components/CodeEditor/editorStore.ts
@@ -1,12 +1,7 @@
 import { signal } from '@preact/signals-core';
 
-export const currentCode = signal('');
 export const currentFile = signal('');
 
-export function updateCode(code: string) {
-	currentCode.value = code;
-}
-
-export function updateFile(file: string) {
-	currentFile.value = file;
+export function setCurrentFile(uuid: string) {
+	currentFile.value = uuid;
 }
